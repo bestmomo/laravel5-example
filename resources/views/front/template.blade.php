daa4a2ab6be50c7f68a9ab98de97d0950c62d791
bestmomo <grandheretique@free.fr>
2014-11-20 00:19:34 +0100

    Tabulation

--- resources/views/front/template.blade.php
+++ resources/views/front/template.blade.php
@@ -27,12 +27,12 @@
 		{!! HTML::style('http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800') !!}
 		{!! HTML::style('http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic') !!}
 
-<style type="text/css">
-	li.imgflag a { padding-left: 15px; }
-	@media screen and (min-width:768px) {
-		li.imgflag a { padding: 30px }
-	}
-</style>
+		<style type="text/css">
+			li.imgflag a { padding-left: 15px; }
+			@media screen and (min-width:768px) {
+				li.imgflag a { padding: 30px }
+			}
+		</style>
 	
 	</head>
 
