74eae62fbdb1044a4aa99ce0dfe288d0c912f7ae
bestmomo <grandheretique@free.fr>
2014-11-20 02:05:09 +0100

    Correct bug on dashboard

--- resources/views/back/index.blade.php
+++ resources/views/back/index.blade.php
@@ -11,19 +11,19 @@
 
   <div class="row">
 
-    @if($nbrMessages)
+    @if(is_int($nbrMessages))
       {!! HTML::panneauAdmin('primary', 'envelope', $nbrMessages, trans('back/admin.new-messages'), 'contact', trans('back/admin.details')) !!}
     @endif
 
-    @if($nbrUsers)
+    @if(is_int($nbrUsers))
       {!! HTML::panneauAdmin('green', 'user', $nbrUsers, trans('back/admin.new-registers'), 'user', trans('back/admin.details')) !!}
     @endif
 
-    @if($nbrPosts)
+    @if(is_int($nbrPosts))
       {!! HTML::panneauAdmin('yellow', 'pencil', $nbrPosts, trans('back/admin.new-posts'), 'blog', trans('back/admin.details')) !!}
     @endif
 
-    @if($nbrComments)
+    @if(is_int($nbrComments))
       {!! HTML::panneauAdmin('red', 'comment', $nbrComments, trans('back/admin.new-comments'), 'comment', trans('back/admin.details')) !!}
     @endif
 
