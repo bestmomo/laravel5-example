7f81666b9fa14a815518a259c95dacfe302e720a
bestmomo <grandheretique@free.fr>
2014-12-03 01:07:47 +0100

    Changes in error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,15 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class Handler extends ExceptionHandler {
 
 	/**
+	 * A list of the exception types that should not be reported.
+	 *
+	 * @var array
+	 */
+	 protected $dontReport = [
+	 	'Symfony\Component\HttpKernel\Exception\HttpException'
+	 ];
+
+	/**
 	 * Report or log an exception.
 	 *
 	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
@@ -20,7 +29,7 @@ class Handler extends ExceptionHandler {
 	}
 
 	/**
-	 * Render an exception into a response.
+	 * Render an exception into an HTTP response.
 	 *
 	 * @param  \Illuminate\Http\Request  $request
 	 * @param  \Exception  $e
@@ -28,9 +37,13 @@ class Handler extends ExceptionHandler {
 	 */
 	public function render($request, Exception $e)
 	{
-		if($e instanceof NotFoundHttpException)
-		{
-			return response()->view('front.missing', [], 404);
+		if ($this->isHttpException($e))
+		{		
+			if($e instanceof NotFoundHttpException)
+			{
+				return response()->view('front.missing', [], 404);
+			}
+			return $this->renderHttpException($e);
 		}
 		return parent::render($request, $e);
 	}
