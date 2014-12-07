5e30c1235b1c3cb1d75859c0e87ae1c2c397d0d3
bestmomo <grandheretique@free.fr>
2014-12-08 00:44:31 +0100

    Refactoring Javascript

--- views/back/blog/template.blade.php
+++ views/back/blog/template.blade.php
@@ -74,23 +74,6 @@
 
 		CKEDITOR.replace( 'contenu', config);
 
-		function sansAccent(str){
-		    var accent = [
-		        /[\300-\306]/g, /[\340-\346]/g, // A, a
-		        /[\310-\313]/g, /[\350-\353]/g, // E, e
-		        /[\314-\317]/g, /[\354-\357]/g, // I, i
-		        /[\322-\330]/g, /[\362-\370]/g, // O, o
-		        /[\331-\334]/g, /[\371-\374]/g, // U, u
-		        /[\321]/g, /[\361]/g, // N, n
-		        /[\307]/g, /[\347]/g // C, c
-		    ];
-		    var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
-		    for(var i = 0; i < accent.length; i++){
-		        str = str.replace(accent[i], noaccent[i]);
-		    }
-		    return str;
-		}
-
     $("#titre").keyup(function(){
 			var str = sansAccent($(this).val());
 			str = str.replace(/[^a-zA-Z0-9\s]/g,"");
