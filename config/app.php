30e115aa140e6b102809dafeda1869857f161e35
bestmomo <colorstonic@free.fr>
2015-01-03 10:27:45 +0100

    Master synchronization

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
 	|
 	*/
 
-	'key' => env('APP_KEY') ?: 'YourSecretKey!!!',
+	'key' => env('APP_KEY', 'YourSecretKey!!!'),
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
@@ -144,7 +144,7 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 		'App\Services\Html\HtmlServiceProvider',
-		
+
 	],
 
 	/*
