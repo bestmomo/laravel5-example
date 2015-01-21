e8e2fb3a9306dce222b5a78d51e78fdbcece91de
bestmomo <colorstonic@free.fr>
2015-01-21 16:41:05 +0100

    Cleaning code

--- app/Http/Requests/RoleRequest.php
+++ app/Http/Requests/RoleRequest.php
@@ -16,14 +16,4 @@ class RoleRequest extends Request {
 		];
 	}
 
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		return true;
-	}
-
 }
