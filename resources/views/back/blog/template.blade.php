5764b4a861a058706461a6988eaa91e114f44888
bestmomo <colorstonic@free.fr>
2015-07-19 22:33:06 +0200

    Correction back blog template

--- resources/views/back/blog/template.blade.php
+++ resources/views/back/blog/template.blade.php
@@ -8,8 +8,8 @@
 
 @section('main')
 
- <!-- Entête de page -->
-  @include('back.partials.entete', ['title' => trans('back/blog.dashboard'), 'icone' => 'pencil', 'fil' => link_to('blog', 'Articles') . ' / ' . trans('back/blog.creation')])
+	<!-- Entête de page -->
+	@include('back.partials.entete', ['title' => trans('back/blog.dashboard'), 'icone' => 'pencil', 'fil' => link_to('blog', 'Articles') . ' / ' . trans('back/blog.creation')])
 
 	<div class="col-sm-12">
 		@yield('form')
@@ -25,7 +25,7 @@
 
 		<div class="form-group {!! $errors->has('slug') ? 'has-error' : '' !!}">
 			{!! Form::label('slug', trans('back/blog.permalink'), ['class' => 'control-label']) !!}
-			{!! url('/') . '/ ' . Form::text('slug', null, ['id' => 'permalien']) !!}
+			{!! url('/') . '/blog/' . Form::text('slug', null, ['id' => 'permalien']) !!}
 			<small class="text-danger">{!! $errors->first('slug') !!}</small>
 		</div>
 
@@ -46,35 +46,35 @@
 	
 	<script>
 
-	  var config = {
-			codeSnippet_theme: 'Monokai',
-			language: '{{ config('app.locale') }}',
-			height: 100,
-			filebrowserBrowseUrl: '{!! url($url) !!}',
-			toolbarGroups: [
-				{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-				{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-				{ name: 'links' },
-				{ name: 'insert' },
-				{ name: 'forms' },
-				{ name: 'tools' },
-				{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-				{ name: 'others' },
-				//'/',
-				{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-				{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-				{ name: 'styles' },
-				{ name: 'colors' }
-			]
-		};
+	var config = {
+		codeSnippet_theme: 'Monokai',
+		language: '{{ config('app.locale') }}',
+		height: 100,
+		filebrowserBrowseUrl: '{!! url($url) !!}',
+		toolbarGroups: [
+			{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+			{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+			{ name: 'links' },
+			{ name: 'insert' },
+			{ name: 'forms' },
+			{ name: 'tools' },
+			{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+			{ name: 'others' },
+			//'/',
+			{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+			{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+			{ name: 'styles' },
+			{ name: 'colors' }
+		]
+	};
 
-    CKEDITOR.replace( 'summary', config);
+	CKEDITOR.replace( 'summary', config);
 
-		config['height'] = 400;		
+	config['height'] = 400;		
 
-		CKEDITOR.replace( 'content', config);
+	CKEDITOR.replace( 'content', config);
 
-    $("#title").keyup(function(){
+	$("#title").keyup(function(){
 			var str = sansAccent($(this).val());
 			str = str.replace(/[^a-zA-Z0-9\s]/g,"");
 			str = str.toLowerCase();
