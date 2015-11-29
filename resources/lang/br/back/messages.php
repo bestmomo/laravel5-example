813494c97152288445518409e5e449d9c339b61d
Ricardo Fontanelli <ricardo@webunion.com.br>
2015-11-29 14:40:19 -0200

    ADD support for multiple languages

---
+++ resources/lang/br/back/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'dashboard' => 'Gestão de Mensagens',
+	'messages' => 'Mensagens',
+	'name' => 'Nome',
+	'email' => 'Email',		
+	'date' => 'Data',
+	'seen' => 'revisado',
+	'destroy' => 'Excluido',
+	'destroy-warning' => 'Realmente destruir esta mensagem ?',
+	'fail' => 'Falha na atualização.',
+];
---
+++ resources/lang/br/front/missing.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'error-404' => 'Erro 404',
+	'info' => 'Esta página não existe !',
+	'button' => 'Home'
+];
